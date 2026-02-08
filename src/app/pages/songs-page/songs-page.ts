7b58df3d591f6e0c310498fbfd7388e88af07007
AguSSantos Prieto Atienza <131393036+aguSSantos023@users.noreply.github.com>
2026-02-08 10:54:29 +0100

    feat: add song manager and list view

--- src/app/pages/songs-page/songs-page.ts
+++ src/app/pages/songs-page/songs-page.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SongManager } from '../../services/song-manager';
+import { environment } from '../../../environments/environment.development';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-songs-page',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './songs-page.html',
   styleUrl: './songs-page.css',
 })
 export class SongsPage {
+  public songManager = inject(SongManager);
 
+  backendUrl = environment.apiUrl.replace('/api', '');
+
+  formatSize(bytes: string): string {
+    const mb = parseInt(bytes) / (1024 * 1024);
+    return mb.toFixed(2) + ' MB';
+  }
 }
---
+++ src/app/services/song-manager.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, resource } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { firstValueFrom, Observable } from 'rxjs';
+import { Song } from '../models/song.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SongManager {
+  private http = inject(HttpClient);
+
+  private apiUrl = `${environment.apiUrl}/songs`;
+
+  songs = resource({
+    loader: () => firstValueFrom(this.http.get<Song[]>(this.apiUrl)),
+  });
+
+  async upload(file: File, title: string) {
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('file', file);
+
+    return await firstValueFrom(this.http.post(this.apiUrl, formData));
+  }
+}
