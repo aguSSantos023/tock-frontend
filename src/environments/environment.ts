912c4637f1dc3e604bf911b62aaff723425d739f
AguSSantos Prieto Atienza <131393036+aguSSantos023@users.noreply.github.com>
2026-02-09 23:01:36 +0100

    Update apiUrl for production environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '/api', // En producción usaremos proxy inverso, así que será relativa
+  apiUrl: 'http://api.164.68.108.131.nip.io', // En producción usaremos proxy inverso, así que será relativa
 };
