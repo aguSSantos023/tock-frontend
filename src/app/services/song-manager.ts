4b276c679ac4824c85ab1aacf87644d84ed7633a
AguSSantos Prieto Atienza <131393036+aguSSantos023@users.noreply.github.com>
2026-03-04 15:51:37 +0100

    feat: stop requests if is all songs loaded

--- src/app/services/song-manager.ts
+++ src/app/services/song-manager.ts
@@ -12,12 +12,14 @@ export class SongManager {
 
   private apiUrl = `${environment.apiUrl}/songs`;
 
-  currentPage = signal(1);
+  currentPage = signal<number>(1);
   songs = signal<Song[]>([]);
-  loading = signal(false);
+  loading = signal<boolean>(false);
+  isAllSongsLoaded = signal<boolean>(false);
   error = signal<string | null>(null);
 
-  async loadMore() {
+  async loadMore(limit: number = 30) {
+    if (this.isAllSongsLoaded()) return; // Evitar peticiones inecesarias
     if (this.loading()) return; // Evitar múltiples peticiones
 
     this.loading.set(true);
@@ -26,8 +28,10 @@ export class SongManager {
     try {
       const page = this.currentPage();
       const data = await firstValueFrom(
-        this.http.get<PaginatedSongs>(`${this.apiUrl}?page=${page}&limit=15`),
+        this.http.get<PaginatedSongs>(`${this.apiUrl}?page=${page}&limit=${limit}`),
       );
+
+      if (data.data.length < limit) this.isAllSongsLoaded.set(true); // Marca que se han cargado todas las canciones
 
       this.songs.update((current) => [...current, ...data.data]);
       this.currentPage.update((p) => p + 1);
@@ -39,6 +43,7 @@ export class SongManager {
   }
 
   async refresh() {
+    this.isAllSongsLoaded.set(false);
     this.songs.set([]);
     this.currentPage.set(1);
     await this.loadMore();
